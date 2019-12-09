5a13149129270501c30e3bd8f1e80d087a1f9d70
MaximeBouton <maxim.bouton@gmail.com>
2019-12-09 14:59:30 -0800

    perfect sensor test

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,3 +11,18 @@ using Test
     @test occlusion_checker(veh1, veh2, obs)
     @test !occlusion_checker(veh1, veh3, obs)
 end
+
+@testset "perfect sensor" begin 
+    sensor = PerfectSensor() 
+    roadway = gen_straight_roadway(1, 100.0)
+
+    ego = Vehicle(VehicleState(VecSE2(20.0, 0.0, 0.0), 5.0), VehicleDef(), 1)
+    veh1 = Vehicle(VehicleState(VecSE2(30.0, 0.0, 0.0), 5.0), VehicleDef(), 2)
+    veh2 = Vehicle(VehicleState(VecSE2(10.0, 0.0, 0.0), 5.0), VehicleDef(), 3)
+    scene = Scene([ego, veh1, veh2])
+
+    obs = measure(sensor, ego, scene, roadway, ConvexPolygon[])
+
+    @test obs[1] == veh1 
+    @test obs[2] == veh2
+end
